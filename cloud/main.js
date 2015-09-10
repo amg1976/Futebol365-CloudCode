5b82e2021c11129bfb30ccade1a9d22eaf256afb
goncalvesa <goncalvesa@ryanair.com>
2015-09-10 22:52:17 +0100

    Inserting / updating Games and Teams, without duplicates

--- cloud/main.js
+++ cloud/main.js
@@ -22,88 +22,99 @@ Parse.Cloud.define("update_tvgames", function(request, response) {
 					allItems.push(item);
 				});
 
-				var query = new Parse.Query(FPTGame);
-				var allGuids = allItems.map(function(element) { return element.guid.text() });
-				query.containedIn("guid", allGuids);
-				query.find({
-					success: function(results) {
-						var itemsToUpdate = Array();
-						var itemsToSave = allItems.filter(function(element1) {
-							var currentGuid = element1.guid.text();
-							var filteredResults = results.filter(function(element2) {
-								return element2.get("guid") == currentGuid;
-							});
-							if (filteredResults.length == 0) {
-								return true;
-							} else {
-								var item = filteredResults[0];
-								item.setTitle(element1.title.text());
-								item.set("link", element1.link.text());
-								itemsToUpdate.push(item);
-								return false;
-							}
-						});
-						for (var i = 0; i < itemsToSave.length; i++) {
-							itemsToSave[i] = FPTGame.newFromXml(itemsToSave[i]);
-						};
-						var itemsToSubmit = itemsToSave.concat(itemsToUpdate);
-						if (itemsToSubmit.length > 0) {
+				var teamsQuery = new Parse.Query(FPTTeam);
+				teamsQuery.limit(1000);
+				teamsQuery.find({
+					success: function(teamResults) {
 
-							var allTeamNames = Array();
-							for (var i = 0; i < itemsToSubmit.length; i++) {
-								allTeamNames = allTeamNames.concat(itemsToSubmit[i].teamNames);
-							}
-							var allUniqueTeams = arrayUnique(allTeamNames);
-
-							for (var i = 0; i < allUniqueTeams.length; i++) {
-								var newTeam = FPTTeam.newWithName(allUniqueTeams[i]);
-								allUniqueTeams[i] = newTeam;
-								//newTeam.save();
-								/*
-								newTeam.save(null, { 
-									success: function(team) {
-										console.log("Created team " + team.get("name"));
-									}, 
-									error: function(team, error) {
-										console.log("Error creating team " + team.get("name") + ": " + error);
+						var query = new Parse.Query(FPTGame);
+						var allGuids = allItems.map(function(element) { return element.guid.text() });
+						query.limit(1000);
+						query.containedIn("guid", allGuids);
+						query.find({
+							success: function(results) {
+								var itemsToUpdate = Array();
+								var itemsToSave = allItems.filter(function(element1) {
+									var currentGuid = element1.guid.text();
+									var filteredResults = results.filter(function(element2) {
+										return element2.get("guid") == currentGuid;
+									});
+									if (filteredResults.length == 0) {
+										return true;
+									} else {
+										var item = filteredResults[0];
+										item.setTitle(element1.title.text());
+										item.set("link", element1.link.text());
+										itemsToUpdate.push(item);
+										return false;
 									}
 								});
-								*/
-							};
+								for (var i = 0; i < itemsToSave.length; i++) {
+									itemsToSave[i] = FPTGame.newFromXml(itemsToSave[i]);
+								};
+								
+								var itemsToSubmit = itemsToSave.concat(itemsToUpdate);
+								
+								if (itemsToSubmit.length > 0) {
 
-							for (var i = 0; i < itemsToSubmit.length; i++) {
-								var game = itemsToSubmit[i];
-								var homeTeam = allUniqueTeams.filter(function(item) {
-									return item.get("name") == game.teamNames[0];
-								})[0];
-								var awayTeam = allUniqueTeams.filter(function(item) {
-									return item.get("name") == game.teamNames[1];
-								})[0];
-								game.set("homeTeam", homeTeam);
-								game.set("awayTeam", awayTeam);
-							}
+									var allTeamNames = Array();
+									for (var i = 0; i < itemsToSubmit.length; i++) {
+										allTeamNames = allTeamNames.concat(itemsToSubmit[i].teamNames);
+									}
+									var allUniqueTeams = arrayUnique(allTeamNames);
 
-							Parse.Object.saveAll(allUniqueTeams, {
-								success: function(teamList) {
-									//response.success("Saved " + teamList.length + " teams");
-									Parse.Object.saveAll(itemsToSubmit, {
-										success: function(list) { response.success("Saved " + list.length + " games, " + teamList.length + " teams"); },
-										error: function(error) { response.error("Error saving: " + error); }
+									for (var i = 0; i < allUniqueTeams.length; i++) {
+										var name = allUniqueTeams[i];
+										var filtered = teamResults.filter(function(item) {
+											return item.get("name") == name;
+										});
+										if (filtered.length == 0) {
+											var newTeam = FPTTeam.newWithName(name);
+											allUniqueTeams[i] = newTeam;
+										} else {
+											allUniqueTeams[i] = filtered[0];
+										}
+									};
+
+									for (var i = 0; i < itemsToSubmit.length; i++) {
+										var game = itemsToSubmit[i];
+										var homeTeam = allUniqueTeams.filter(function(item) {
+											return item.get("name") == game.teamNames[0];
+										})[0];
+										var awayTeam = allUniqueTeams.filter(function(item) {
+											return item.get("name") == game.teamNames[1];
+										})[0];
+										game.set("homeTeam", homeTeam);
+										game.set("awayTeam", awayTeam);
+									}
+
+									Parse.Object.saveAll(allUniqueTeams, {
+										success: function(teamList) {
+											Parse.Object.saveAll(itemsToSubmit, {
+												success: function(list) { response.success("Saved " + list.length + " games, " + teamList.length + " teams"); },
+												error: function(error) { response.error("Error saving: " + error); }
+											});
+										},
+										error: function(error) {
+											response.log("Error saving teams: " + error);
+										}
 									});
-								},
-								error: function(error) {
-									response.log("Error saving teams: " + error);
-								}
-							});
 
-						} else {
-							response.success("No items to save");
-						}
+								} else {
+									response.success("No items to save");
+								}
+							},
+							error: function(error) {
+								response.error("Error in query: " + error);
+							}
+						});
+
 					},
 					error: function(error) {
-						response.error("Error in query: " + error);
+						response.error("Error in teams query: " + error);
 					}
 				});
+
 			}
 		})
 	},function(httpResponse) {
