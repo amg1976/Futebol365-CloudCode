a356985cae5b9ee7f8b138a88ef2e29b4b2089f6
goncalvesa <goncalvesa@ryanair.com>
2015-09-19 18:12:24 +0100

    Refactored games array handling code to specific array wrapper

--- cloud/main.js
+++ cloud/main.js
@@ -4,6 +4,54 @@ var FPTGame = game.create();
 var team = require("cloud/FPTTeam.js")
 var FPTTeam = team.create();
 
+function makeGamesList() {
+
+	var arr = Array();
+	arr.push.apply(arr, arguments);
+
+	arr.loadFromRSSResponse = function(xmlObject) {
+		var ownArray = this;
+		xmlObject.rss.channel.item.each(function(index, item) { 
+			ownArray.push(item);
+		});
+	}
+
+	arr.convertToFPTGames = function(queryResults) {
+
+		var ownArray = this;
+		for (var i = 0; i < ownArray.length; i++) {
+
+			var currentGuid = ownArray[i].guid.text();
+			var filteredResults = queryResults.filter(function(element2) {
+				return element2.get("guid") == currentGuid;
+			});
+			if (filteredResults.length == 0) {
+				ownArray[i] = FPTGame.newFromXml(ownArray[i]);
+			} else {
+				var item = filteredResults[0];
+				item.setTitle(ownArray[i].title.text());
+				item.set("link", ownArray[i].link.text());
+				ownArray[i] = item;
+			}
+
+		}
+
+	}
+
+	arr.getUniqueTeamNames = function() {
+
+		var allTeamNames = Array();
+		for (var i = 0; i < arr.length; i++) {
+			allTeamNames = allTeamNames.concat(arr[i].teamNames);
+		}
+		return arrayUnique(allTeamNames);
+
+	}
+
+	return arr;
+
+}
+
 Parse.Cloud.job("update_tvgames", function(request, response) {
 	Parse.Cloud.useMasterKey();
 	Parse.Cloud.httpRequest({
@@ -14,10 +62,8 @@ Parse.Cloud.job("update_tvgames", function(request, response) {
 			if (error) {
 				response.error(error);
 			} else {
-				var allItems = Array();
-				result.rss.channel.item.each(function(index, item) { 
-					allItems.push(item);
-				});
+				var allItems = makeGamesList();
+				allItems.loadFromRSSResponse(result);
 
 				var query = new Parse.Query(FPTGame);
 				var allGuids = allItems.map(function(element) { return element.guid.text() });
@@ -25,35 +71,12 @@ Parse.Cloud.job("update_tvgames", function(request, response) {
 				query.containedIn("guid", allGuids);
 				query.find({
 					success: function(results) {
-						var itemsToUpdate = Array();
-						var itemsToSave = allItems.filter(function(element1) {
-							var currentGuid = element1.guid.text();
-							var filteredResults = results.filter(function(element2) {
-								return element2.get("guid") == currentGuid;
-							});
-							if (filteredResults.length == 0) {
-								return true;
-							} else {
-								var item = filteredResults[0];
-								item.setTitle(element1.title.text());
-								item.set("link", element1.link.text());
-								itemsToUpdate.push(item);
-								return false;
-							}
-						});
-						for (var i = 0; i < itemsToSave.length; i++) {
-							itemsToSave[i] = FPTGame.newFromXml(itemsToSave[i]);
-						};
-						
-						var itemsToSubmit = itemsToSave.concat(itemsToUpdate);
-						
-						if (itemsToSubmit.length > 0) {
 
-							var allTeamNames = Array();
-							for (var i = 0; i < itemsToSubmit.length; i++) {
-								allTeamNames = allTeamNames.concat(itemsToSubmit[i].teamNames);
-							}
-							var allUniqueTeams = arrayUnique(allTeamNames);
+						allItems.convertToFPTGames(results);
+						
+						if (allItems.length > 0) {
+
+							var allUniqueTeams = allItems.getUniqueTeamNames();
 
 							var teamsQuery = new Parse.Query(FPTTeam);
 							teamsQuery.limit(1000);
@@ -74,8 +97,8 @@ Parse.Cloud.job("update_tvgames", function(request, response) {
 										}
 									};
 
-									for (var i = 0; i < itemsToSubmit.length; i++) {
-										var game = itemsToSubmit[i];
+									for (var i = 0; i < allItems.length; i++) {
+										var game = allItems[i];
 										var homeTeam = allUniqueTeams.filter(function(item) {
 											return item.get("name") == game.teamNames[0];
 										})[0];
@@ -90,7 +113,7 @@ Parse.Cloud.job("update_tvgames", function(request, response) {
 
 									Parse.Object.saveAll(allUniqueTeams, {
 										success: function(teamList) {
-											Parse.Object.saveAll(itemsToSubmit, {
+											Parse.Object.saveAll(allItems, {
 												success: function(list) { response.success("Saved " + list.length + " games, " + teamList.length + " teams"); },
 												error: function(error) { response.error("Error saving: " + error); }
 											});
